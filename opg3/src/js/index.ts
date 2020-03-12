2ac3b1556ff169faf24e008332a161293dd8e2ae
Franco9696 <48202566+Franco9696@users.noreply.github.com>
2020-03-12 15:06:40 +0100

    Add files via upload
    opgave 3 færdig

---
+++ opg3/src/js/index.ts
@@ -0,0 +1,27 @@
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+let buttonElement: HTMLButtonElement = <HTMLButtonElement>document.getElementById("Shift")
+buttonElement.addEventListener("click", textChanger);
+
+function textChanger(): void {
+    let inPutElement: HTMLInputElement = <HTMLInputElement>document.getElementById("TextInput");
+    let inputString: string = inPutElement.value;
+
+
+    let selectElement: HTMLSelectElement = <HTMLSelectElement>document.getElementById("Selector");
+    let Selector: string = selectElement.value;
+
+    let resultElement: HTMLParagraphElement = <HTMLParagraphElement>document.getElementById("output");
+
+    if (Selector == "Uppercase") {
+        let result = inputString.toUpperCase();
+        resultElement.innerHTML = result;
+    }
+    else if (Selector == "Lowercase") {
+        let result = inputString.toLowerCase();
+        resultElement.innerHTML = result;
+    }
+}
